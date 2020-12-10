cb644df95d9bf8758f7632e0a3430c9a5521c318
eykcheung <eykcheung@gmail.com>
2020-12-10 11:05:53 +0800

    stock removable and input elements editable

--- script.js
+++ script.js
@@ -12,69 +12,210 @@ var Portfolio = function (_React$Component) {
   function Portfolio(props) {
     _classCallCheck(this, Portfolio);
 
-    return _possibleConstructorReturn(this, (Portfolio.__proto__ || Object.getPrototypeOf(Portfolio)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Portfolio.__proto__ || Object.getPrototypeOf(Portfolio)).call(this, props));
+
+    _this.state = {
+      portfolio: [{
+        name: 'Feetbook',
+        shares_owned: 20,
+        cost_per_share: 50,
+        market_price: 130
+      }, {
+        name: 'Yamazon',
+        shares_owned: 5,
+        cost_per_share: 200,
+        market_price: 500
+      }, {
+        name: 'Snoozechat',
+        shares_owned: 100,
+        cost_per_share: 20,
+        market_price: 3
+      }]
+    };
+    // Note: api JSON data often come in underscore_styled like above
+    _this.removeStock = _this.removeStock.bind(_this);
+    _this.handleChange = _this.handleChange.bind(_this);
+    return _this;
   }
 
   _createClass(Portfolio, [{
-    key: "render",
+    key: 'removeStock',
+    value: function removeStock(index) {
+      var portfolio = this.state.portfolio.slice(); //shgallow copy
+      portfolio.splice(index, 1); //remove value at index
+
+      this.setState({ portfolio: portfolio });
+    }
+  }, {
+    key: 'handleChange',
+    value: function handleChange(event, index) {
+      var portfolio = this.state.portfolio.slice(); //shallow copy
+      var _event$target = event.target,
+          name = _event$target.name,
+          value = _event$target.value;
+
+
+      portfolio[index][name] = value;
+      this.setState({ portfolio: portfolio });
+    }
+  }, {
+    key: 'render',
     value: function render() {
+      var _this2 = this;
+
+      var portfolio = this.state.portfolio;
+
+
+      var portfolio_market_value = portfolio.reduce(function (sum, stock) {
+        return stock.shares_owned * stock.market_price + sum;
+      }, 0);
+      var portfolio_cost = portfolio.reduce(function (sum, stock) {
+        return stock.shares_owned * stock.cost_per_share + sum;
+      }, 0);
+      var portfolio_gain_loss = portfolio_market_value - portfolio_cost;
+
       return React.createElement(
-        "div",
-        { className: "container" },
+        'div',
+        { className: 'container' },
         React.createElement(
-          "h1",
-          { className: "text-center my-4" },
-          "Stock Portfolio"
+          'h1',
+          { className: 'text-center my-4' },
+          'Stock Portfolio'
         ),
         React.createElement(
-          "div",
-          { className: "row" },
+          'div',
+          { className: 'row' },
           React.createElement(
-            "div",
-            { className: "col-12" },
+            'div',
+            { className: 'col-12' },
             React.createElement(
-              "table",
-              { className: "table table-responsive" },
+              'table',
+              { className: 'table table-responsive' },
               React.createElement(
-                "thead",
+                'thead',
                 null,
                 React.createElement(
-                  "tr",
+                  'tr',
                   null,
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Name"
+                    'th',
+                    { scope: 'col' },
+                    'Name'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Shares Owned"
+                    'th',
+                    { scope: 'col' },
+                    'Shares Owned'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Cost per share ($)"
+                    'th',
+                    { scope: 'col' },
+                    'Cost per share ($)'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Market Price ($)"
+                    'th',
+                    { scope: 'col' },
+                    'Market Price ($)'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Market Value ($)"
+                    'th',
+                    { scope: 'col' },
+                    'Market Value ($)'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Unrealized Gain/Loss ($)"
+                    'th',
+                    { scope: 'col' },
+                    'Unrealized Gain/Loss ($)'
                   ),
-                  React.createElement("th", { scope: "col" })
+                  React.createElement('th', { scope: 'col' })
                 )
               ),
-              React.createElement("tbody", null)
+              React.createElement(
+                'tbody',
+                null,
+                portfolio.map(function (stock, index) {
+                  var name = stock.name,
+                      shares_owned = stock.shares_owned,
+                      cost_per_share = stock.cost_per_share,
+                      market_price = stock.market_price;
+
+
+                  var market_value = shares_owned * market_price;
+                  var unrealized_gain_loss = market_value - shares_owned * cost_per_share;
+
+                  return React.createElement(
+                    'tr',
+                    { key: index },
+                    React.createElement(
+                      'td',
+                      null,
+                      name
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      React.createElement('input', { onChange: function onChange(e) {
+                          return _this2.handleChange(e, index);
+                        }, type: 'number', name: 'shares_owned', value: shares_owned })
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      React.createElement('input', { onChange: function onChange(e) {
+                          return _this2.handleChange(e, index);
+                        }, type: 'number', name: 'cost_per_share', value: cost_per_share })
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      React.createElement('input', { onChange: function onChange(e) {
+                          return _this2.handleChange(e, index);
+                        }, type: 'number', name: 'market_price', value: market_price })
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      market_value
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      unrealized_gain_loss
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      React.createElement(
+                        'button',
+                        { className: 'btn btn-light btn-sm', onClick: function onClick() {
+                            return _this2.removeStock(index);
+                          } },
+                        'remove'
+                      )
+                    )
+                  );
+                })
+              )
+            )
+          ),
+          React.createElement(
+            'div',
+            { className: 'col-12 col-md-6' },
+            React.createElement(
+              'h4',
+              { className: 'mb-3' },
+              'Portfolio value: $',
+              portfolio_market_value
+            )
+          ),
+          React.createElement(
+            'div',
+            { 'class': 'col-12 col-md-6' },
+            React.createElement(
+              'h4',
+              { className: 'mb-3' },
+              'Portfolio gain/loss: $',
+              portfolio_gain_loss
             )
           )
         )
--- src/script.js
+++ src/script.js
@@ -1,8 +1,55 @@
 class Portfolio extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      portfolio: [
+        {
+          name: 'Feetbook',
+          shares_owned: 20,
+          cost_per_share: 50,
+          market_price: 130
+        },{
+          name: 'Yamazon',
+          shares_owned: 5,
+          cost_per_share: 200,
+          market_price: 500
+        },{
+          name: 'Snoozechat',
+          shares_owned: 100,
+          cost_per_share: 20,
+          market_price: 3
+        }
+      ]
+    };
+    // Note: api JSON data often come in underscore_styled like above
+    this.removeStock = this.removeStock.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  removeStock(index) {
+    const portfolio = this.state.portfolio.slice(); //shgallow copy
+    portfolio.splice(index, 1); //remove value at index
+
+    this.setState({ portfolio });
+  }
+
+  handleChange(event, index) {
+    const portfolio = this.state.portfolio.slice(); //shallow copy
+    const { name, value } = event.target;
+
+    portfolio[index][name] = value;
+    this.setState({portfolio});
+  }
+
   render() {
+    const { portfolio } = this.state;
+
+    const portfolio_market_value = portfolio.reduce((sum, stock) =>
+  stock.shares_owned * stock.market_price + sum, 0);
+    const portfolio_cost = portfolio.reduce((sum, stock) =>
+  stock.shares_owned * stock.cost_per_share + sum, 0);
+    const portfolio_gain_loss = portfolio_market_value - portfolio_cost;
+
     return (
       <div className="container">
         <h1 className="text-center my-4">Stock Portfolio</h1>
@@ -21,8 +68,38 @@ class Portfolio extends React.Component {
                 </tr>
               </thead>
               <tbody>
+                {portfolio.map((stock, index) => {
+                  const {
+                    name,
+                    shares_owned,
+                    cost_per_share,
+                    market_price,
+                  } = stock;
+
+                  const market_value = shares_owned * market_price;
+                  const unrealized_gain_loss = market_value - shares_owned * cost_per_share;
+
+                  return (
+                    <tr key ={index}>
+                      <td>{name}</td>
+                      <td><input onChange={e => this.handleChange(e, index)} type="number" name="shares_owned" value={shares_owned} /></td>
+                      <td><input onChange={e => this.handleChange(e, index)} type="number" name="cost_per_share" value={cost_per_share} /></td>
+                      <td><input onChange={e => this.handleChange(e, index)} type="number" name="market_price" value={market_price} /></td>
+                      <td>{market_value}</td>
+                      <td>{unrealized_gain_loss}</td>
+                      <td><button className="btn btn-light btn-sm" onClick={() => this.removeStock(index)}>remove</button></td>
+                    </tr>
+                  )
+
+                })}
               </tbody>
             </table>
+          </div>
+          <div className = "col-12 col-md-6">
+            <h4 className = "mb-3">Portfolio value: ${portfolio_market_value}</h4>
+          </div>
+          <div class = "col-12 col-md-6">
+            <h4 className = "mb-3">Portfolio gain/loss: ${portfolio_gain_loss}</h4>
           </div>
         </div>
       </div>
